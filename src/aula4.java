5da9f329106ba817225daf916978e59e4db80a62
Wesley Spengler <wesley63466796@alunos.sc.senac.br>
2026-04-02 15:45:32 -0300

    Exercicio 1 verifica se é par ou impar. Exercicio dois identifica o maior entre os dois. Inclui entrada de dados pelo usuario via Scanner.

--- src/aula4.java
+++ src/aula4.java
@@ -1,9 +1,51 @@
+import java.util.Scanner;
 
 public class aula4 {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//atividade1();
+		atividade2();
+		
+	
 	}
+		     
 
+     static void atividade1() {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("Par");
+        } else {
+            System.out.println("Ímpar");
+        }
+
+        sc.close();
+    }
+
+
+	static void atividade2() {
+		Scanner sc = new Scanner(System.in);
+
+	    System.out.print("Digite o primeiro número: ");
+	    int a = sc.nextInt();
+
+	    System.out.print("Digite o segundo número: ");
+	    int b = sc.nextInt();
+
+	    if (a > b) {
+	        System.out.println("Maior: " + a);
+	    } else {
+	        System.out.println("Maior: " + b);
+	    }
+
+	    sc.close();
+    }
+	
+	
+	
+	
 }
